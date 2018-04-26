20ca6b532f2207fb17288ab8d921b15c1375dfcd
asghimire <37714894+asghimire@users.noreply.github.com>
2018-04-26 11:38:16 -0400

    change make report code

--- src/make_report.py
+++ src/make_report.py
@@ -2,8 +2,11 @@ from src.msds510.util import get_top_ten_avengers
 
 top_avengers = get_top_ten_avengers(r'C:\msds510\data\processed\avengers_processed.csv')
 
-for index in range(len(top_avengers)):
+for index in range(0,len(top_avengers)):
+    print((index+1), '.', top_avengers[index]['Name/Alias'])
+    print('                      '
+          '                       ')
     for key in top_avengers[index]:
-        print(key,':', top_avengers[index][key])
+        print('*', key,':', top_avengers[index][key])
     print('-----------------------'
           '-----------------------')
