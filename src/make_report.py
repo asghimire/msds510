a58349fdc26ec32d8df6588f7960df1088306c2a
asghimire <37714894+asghimire@users.noreply.github.com>
2018-04-28 09:11:56 -0400

    s

--- src/make_report.py
+++ src/make_report.py
@@ -5,9 +5,14 @@ top_avengers = get_top_ten_avengers(r'C:\msds510\data\processed\avengers_process
 for i in range(0,len(top_avengers)):
     print((i+1), '.', top_avengers[i]['name_alias'])
     print()
-
-    for key in top_avengers[i]:
-        top_avengers[i]['name_alias']=top_avengers[i].pop('name_alias')
-        print('*', key,':', top_avengers[i][key])
-
+    print('*', 'Number of Appearances', ':', top_avengers[i]['appearances'])
+    print('*', 'Year Joined', ':', top_avengers[i]['year'])
+    print('*', 'Years Since Joining', ':', top_avengers[i]['years_since_joining'])
+    print('*', 'URL', ':', top_avengers[i]['url'])
     print()
+    print('## Notes')
+    print()
+    print(top_avengers[i]['notes'])
+    print()
+
+
