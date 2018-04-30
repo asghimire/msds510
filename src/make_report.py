032b9e20aed73e81cf8f66845fe419fd4fbfa4e0
asghimire <37714894+asghimire@users.noreply.github.com>
2018-04-30 07:29:45 -0400

    h

--- src/make_report.py
+++ src/make_report.py
@@ -1,18 +1,5 @@
-from src.msds510.util import get_top_ten_avengers
-
-top_avengers = get_top_ten_avengers(r'C:\msds510\data\processed\avengers_processed.csv')
-
-for i in range(0,len(top_avengers)):
-    print((i+1), '.', top_avengers[i]['name_alias'])
-    print()
-    print('*', 'Number of Appearances', ':', top_avengers[i]['appearances'])
-    print('*', 'Year Joined', ':', top_avengers[i]['year'])
-    print('*', 'Years Since Joining', ':', top_avengers[i]['years_since_joining'])
-    print('*', 'URL', ':', top_avengers[i]['url'])
-    print()
-    print('## Notes')
-    print()
-    print(top_avengers[i]['notes'])
-    print()
+import subprocess
+with open(r'C:\msds510\reports\top_ten_appearances.md', 'w') as write_to_report:
+    subprocess.call(['python', r'C:\msds510\src\msds510\util.py'], stdout=write_to_report )
 
 
--- src/msds510/util.py
+++ src/msds510/util.py
@@ -15,5 +15,25 @@ def get_top_ten_avengers(inputfile):
         temp = dict((k, i[k]) for k in desired_fields if k in i)
         new_top_ten_record.append(temp)
     return (new_top_ten_record)
+
+
+ top_avengers = get_top_ten_avengers(r'C:\msds510\data\processed\avengers_processed.csv')
+
+def print_report(filepath):
+ # if __name__ == "__main__":
+ top_avengers = get_top_ten_avengers(filepath)
+ for i in range(0, len(top_avengers)):
+     print('#', (i + 1), '.', top_avengers[i]['name_alias'])
+     print()
+     print('*', 'Number of Appearances', ':', top_avengers[i]['appearances'])
+     print('*', 'Year Joined', ':', top_avengers[i]['year'])
+     print('*', 'Years Since Joining', ':', top_avengers[i]['years_since_joining'])
+     print('*', 'URL', ':', top_avengers[i]['url'])
+     print()
+     print('## Notes')
+     print()
+     print(top_avengers[i]['notes'])
+     print()
+
 if __name__ == "__main__":
-    print(get_top_ten_avengers(r'C:\msds510\data\processed\avengers_processed.csv'))
+    print_report(r'C:\msds510\data\processed\avengers_processed.csv')
